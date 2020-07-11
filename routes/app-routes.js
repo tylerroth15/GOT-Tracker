b7b957cd61156d151b022d53f7dd4d29659e093c
Tyler Roth <tylerroth1535@gmail.com>
2020-07-11 13:41:59 -0400

    fixing lint errors and added new photos

--- routes/app-routes.js
+++ routes/app-routes.js
@@ -4,41 +4,42 @@ const db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
-    // Search for Specific Character (or all characters) then provides JSON
-    app.get("/api/:person?", function(req, res) {
-      if (req.params.person) {
-        db.Person.findOne({
-          where: {
-              //seach function
-            routeName: req.params.person
-          }
-        }).then(function(result) {
-          return res.json(result);
-        });
-      } else {
-        db.Person.findAll().then(function(result) {
-          return res.json(result);
-        });
-      }
-    });
-  
-    // Add A New Character
-    app.post("/api/new", function(req, res) {
-        const person = req.body;
-
-      // Then add the character to the database using sequelize
-     db.Person.create({
-        person: person.person,
-        title: person.title,
-        livingDead: person.livingDead,
-      }).then(function(dbperson){
-          res.json(dbperson);
-      }).catch(function(error){
-          res.status(422).send(error);
+  // Search for Specific Character (or all characters) then provides JSON
+  app.get("/api/:person?", (req, res) => {
+    if (req.params.person) {
+      db.Person.findOne({
+        where: {
+          //seach function
+          routeName: req.params.person
+        }
+      }).then(result => {
+        return res.json(result);
       });
-    });
-    
-    //put
-    app.put()
-  };
+    } else {
+      db.Person.findAll().then(result => {
+        return res.json(result);
+      });
+    }
+  });
 
+  // Add A New Character
+  app.post("/api/new", (req, res) => {
+    const person = req.body;
+
+    // Then add the character to the database using sequelize
+    db.Person.create({
+      person: person.person,
+      title: person.title,
+      livingDead: person.livingDead
+    })
+      .then(dbperson => {
+        res.json(dbperson);
+      })
+      .catch(error => {
+        res.status(422).send(error);
+      });
+  });
+
+  //put
+  app.put();
+};
