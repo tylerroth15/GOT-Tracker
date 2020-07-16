6f342788bbd724aeb3c70b76e084e425b48cecfb
Michael Spikes <michael.spikes85@gmail.com>
2020-07-15 21:24:12 -0400

    signup logging to DB

--- public/js/signup.js
+++ public/js/signup.js
@@ -24,13 +24,12 @@ $(document).ready(() => {
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   function signUpUser(email, password) {
-    $.post("/api/signup", {
+    $.post("/signup", {
       email: email,
       password: password
     })
-      .then(() => {
-        window.location.replace("/members");
-        // If there's an error, handle it by throwing up a bootstrap alert
+      .then(res => {
+        res.redirect("/members");
       })
       .catch(handleLoginErr);
   }
