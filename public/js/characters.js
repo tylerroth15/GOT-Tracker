82b9712e42af0e48ab2d9c13d93f21e5803017c8
Tyler Roth <tylerroth1535@gmail.com>
2020-07-17 16:21:15 -0400

    update character.js and handlebars to attempt to update DB

--- public/js/characters.js
+++ public/js/characters.js
@@ -1,3 +1,31 @@
 $(document).ready(() => {
   $.get("/people").then(res => console.log(res));
 });
+
+// When user clicks update-btn
+$("#update-btn").on("click", event => {
+  event.preventDefault();
+
+  // Update Character object
+  const updateChar = {
+    alive: $("#livingDead")
+      .val()
+      .trim(),
+    house: $("#house")
+      .val()
+      .trim(),
+    title: $("#title")
+      .val()
+      .trim(),
+    loyalty: $("#loyalty")
+      .val()
+      .trim()
+  };
+  // Send an AJAX POST-request with jQuery
+  $.post("/api/new", updateChar)
+    // On success, run the following code
+    .then(data => {
+      // Log the data we found
+      console.log(data);
+    });
+});
